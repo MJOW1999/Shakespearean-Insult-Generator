e78578fc38a58820328e7e7becca51dbeb95098b
michael.jow.williams@gmail.com <michael.jow.williams@gmail.com>
2021-03-30 19:17:35 +0100

    Output insults

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ const beginning = ["artless","bawdy","beslubbering", "bootless", "churlish", "co
 const middle = ["base-court", "bat-fowling", "beef-witted", "beetle-headed", "boil-brained", "clapper-clawed", "clay-brained", "common-kissing", "crook-patted", "dismal-dreaming", "dizzy-eyed", "doghearted", "dread-bolted", "earth-vexing", "elf-skinned", "fat-kidneyed", "fen-sucked", "flap-mouthed", "fly-bitten", "folly-flatten", "fool-born", "full-gorged", "guta-gripping", "half-faced", "hasty-witted", "hedge-born", "hell-hated", "idle-headed", "ill-breeding", "ill-nurtured", "knotty-patted", "milk-livered", "motley-minded", "onion-eyed", "plume-plucked", "pottle-deep", "pox-marked", "reeling-ripe", "rough-hewn", "rude-growing", "rump-fed", "shard-borne", "sheep-biting", "spur-galled", "swag-bellied", "tardy-gaited", "tickle-brained", "toad-spotted", "unchin-snouted", "weather-bitten"];
 //Final part of the insult
 const finish = ["apple-john", "baggage", "barnacle", "bladder", "boar-pig", "bugbear", "bum-bailey", "canker-blossom", "clack-dish", "clotpole", "coxcomb", "codpiece", "death-token", "dewberry", "flap-dragon", "flax-wench", "flirt-gill", "foot-licker", "fustilarian", "giglet", "gudgeon", "haggard", "harpy", "hedge-pig", "horn-beast", "hugger-mugger", "jolthead", "lewdster", "lout", "maggot-pie", "malt-worm", "mammet", "measle", "minnow", "miscreant", "moldwarp", "mumble-newn", "nut-hook", "pigeon-egg", "pignut", "puttock", "pumpion", "ratsbane", "scut", "skainsmate", "strumpet", "varlot", "vassal", "whey-face", "wagtail"];
+
+document.getElementById("new-insult").addEventListener("click", getRandom);
+function getRandom(){
+  var insult = "Thou " + beginning[Math.floor(Math.random() * beginning.length)] + " " + middle[Math.floor(Math.random() * middle.length)] + " " + finish[Math.floor(Math.random() * finish.length)] + "!";
+  document.getElementById("output").innerHTML = insult;
+}
